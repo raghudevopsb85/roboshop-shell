831f0d16e3f554a1e934927e232032e83c9edf57
Raghu K <raghuk.vit@gmail.com>
2025-06-24 08:12:18 +0530

    RoboShop Code

--- user.sh
+++ user.sh
@@ -1,18 +1,4 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+source common.sh
+component=user
+NODEJS
 
-cp user.service /etc/systemd/system/user.service
-
-useradd roboshop
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-
-
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
