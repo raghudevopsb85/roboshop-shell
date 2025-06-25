b3fe8849fc0a2c3b565b427c560ccc894fb29128
Raghu K <raghuk.vit@gmail.com>
2025-06-25 08:10:16 +0530

    RoboShop Code

--- shipping.sh
+++ shipping.sh
@@ -8,7 +8,7 @@ component=shipping
 MAVEN
 
 dnf install mysql -y
-mysql -h mysql-dev.rdevopsb85.online -uroot -p$1 < /app/db/schema.sql
-mysql -h mysql-dev.rdevopsb85.online -uroot -p$1 < /app/db/app-user.sql
-mysql -h mysql-dev.rdevopsb85.online -uroot -p$1 < /app/db/master-data.sql
+for file in schema app-user master-data; do
+  mysql -h mysql-dev.rdevopsb85.online -uroot -p$1 < /app/db/$file.sql
+done
 
